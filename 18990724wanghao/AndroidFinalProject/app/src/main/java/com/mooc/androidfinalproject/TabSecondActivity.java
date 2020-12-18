8ec655335a0c2cd5e957b9c80609ff03fc792931
SENvY-wh <zuotong@hotmail.com>
2020-12-18 20:08:19 +0800

    大作业提交

---
+++ 18990724wanghao/AndroidFinalProject/app/src/main/java/com/mooc/androidfinalproject/TabSecondActivity.java
@@ -0,0 +1,15 @@
+package com.mooc.androidfinalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TabSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tab_second);
+
+    }
+}//These codes are from Peter Pang.
